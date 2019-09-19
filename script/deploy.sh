349909fb2f4046daa3801387c581fa4e8214ba5b
skyedat <stevekye@gmail.com>
2019-09-19 14:09:12 -0700

    tweak container name
    ..

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,5 +3,5 @@ set -e
 
 bash -c "echo \"${DOCKER_PASSWORD}\" | docker login --username \"${DOCKER_LOGIN}\" --password-stdin ;"
 docker pull zutherb/monolithic-shop
-docker tag zutherb/monolithic-shop:latest pochadat/monolithic:latest
-docker push pochadt/monolithic:latest
+docker tag zutherb/monolithic-shop:latest pochadat/monolithic-shop:latest
+docker push pochadt/monolithic-shop:latest
